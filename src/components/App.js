42cc1191c349b78c4fc5292e1fdd1935e51f563f
Jacksen Nillson <nillsonjacksen@gmail.com>
2023-01-09 10:24:54 +0800

    My Finished code for WDS Recipe App

---
+++ src/components/App.js
@@ -0,0 +1,123 @@
+import React, { useState, useEffect } from 'react';
+import '../css/app.css'
+import RecipeList from './RecipeList';
+import RecipeEdit from './RecipeEdit';
+import { v4 as uuidv4 } from 'uuid';
+
+export const RecipeContext = React.createContext();
+const LOCAL_STORAGE_KEY = 'cookingWithReact.recipes';
+
+function App() {
+  const [selectedRecipeId, setSelectedRecipeId] = useState()
+  const [recipes, setRecipes] = useState(() => { 
+    const recipeJSON = localStorage.getItem(LOCAL_STORAGE_KEY)
+    if (recipeJSON == null) {
+      return sampleRecipes
+    } else {
+      return JSON.parse(recipeJSON)
+    }
+  })
+  
+  const selectedRecipe = recipes.find(recipe => recipe.id === selectedRecipeId)
+  
+  useEffect(() => {
+    console.log('Rendered')
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(recipes))
+  }, [recipes])
+  
+  const recipeContextValue = {
+    handleRecipeAdd: handleRecipeAdd,
+    handleRecipeDelete: handleRecipeDelete,
+    handleRecipeSelect: handleRecipeSelect,
+    handleRecipeChange: handleRecipeChange
+  }
+
+  function handleRecipeSelect(id) {
+    setSelectedRecipeId(id)
+  }
+
+  function handleRecipeAdd() {
+    const newRecipe = {
+      id: uuidv4(),
+      name: '',
+      servings: 1,
+      cookTime: '',
+      instructions: '',
+      ingredients: [
+        { id: uuidv4(), name: '', amount: '' }
+      ]
+    }
+  
+    setSelectedRecipeId(newRecipe.id)
+    setRecipes([...recipes, newRecipe])
+  }
+
+  function handleRecipeChange(id, recipe) {
+    const newRecipes = [...recipes]
+    const index = newRecipes.findIndex(r => r.id === id)
+    newRecipes[index] = recipe
+    setRecipes(newRecipes)
+  }
+
+  function handleRecipeDelete(id) {
+    if (selectedRecipeId != null && selectedRecipeId === id ) {
+      setSelectedRecipeId(undefined)
+    }
+    setRecipes(recipes.filter(recipe => recipe.id !== id))
+  }
+
+  return (
+    <RecipeContext.Provider value={recipeContextValue}>
+      <RecipeList recipes={recipes} />
+      {selectedRecipe && <RecipeEdit recipe={selectedRecipe} />}
+    </RecipeContext.Provider>
+  )
+}
+
+
+
+const sampleRecipes = [
+  {
+    id: 1,
+    name: 'Plain Chicken',
+    servings: 3,
+    cookTime: '1:45',
+    instructions: '1. Put salt on chicken\n2. Put chicken in oven\n3. Eat chicken\n',
+    ingredients: [
+      {
+        id: 1,
+        name: 'Chicken',
+        amount: '2 pounds'
+      },
+      {
+        id: 2,
+        name: 'Salt',
+        amount: '1 Tbs'
+      }
+    ]
+  },
+  {
+    id: 2,
+    name: 'Plain Pork',
+    servings: 5,
+    cookTime: '0:45',
+    instructions: '1. Put paprika on pork\n2. Put pork in oven\n3. Eat pork\n',
+    ingredients: [
+      {
+        id: 1,
+        name: 'Pork',
+        amount: '3 pounds'
+      },
+      {
+        id: 2,
+        name: 'Paprika',
+        amount: '2 Tbs'
+      }
+    ]
+  }
+
+]
+
+export default App;
+
+
